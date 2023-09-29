e96620c056bbc9b346e9ff1a0f94ed60e2ddc5df
SharmilaPalanisamy <sharmila.palanisamy@mallow-tech.com>
2023-09-29 10:35:28 +0530

    Render 1

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,10 @@
 class MoviesController < ApplicationController
   def index
+    if params[:commit]=='search'&&params[:search].blank? && (params[:from_date].blank? || params[:to_date].blank?)
+      flash.now[:warning] = 'Please enter at least one search parameter.'
+    end
     @pagy,@movies = pagy(Movie.search_by_name(params[:search])
                               .filter_by_release_date(params[:from_date], params[:to_date])
                               .order_by_highest_rating)
-    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,24 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  #association
+  # Association
   has_many :reviews,dependent: :destroy
+
+  # Validation
+  validate :email_format
+  validate :strong_password
+
+  private
+
+  def email_format
+    unless email.match?(/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/)
+      errors.add(:email, 'address is not valid')
+    end
+  end
+
+  def strong_password
+    unless password.match?(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{6,}$/)
+      errors.add(:password, 'must include at least one uppercase letter, one lowercase letter, and one digit')
+    end
+  end
 end
